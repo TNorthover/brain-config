68998eda93a06e2fabc642e9a67d0b3a903e41bf
Tim Northover <t.p.northover@gmail.com>
2017-08-26 14:40:25 -0700

    Minimally working prototype.
    It can read or write low-level configuration parameters, and has at
    least the semblance of a CLI rather than needing direct Python
    wrangling.

---
+++ brain-config.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import argparse, brain, sys
+
+
+def read_item(brain, item):
+    print('{}: {}'.format(item, brain[item]))
+
+def write_item(brain, str):
+    if str.count('=') != 1:
+        raise ValueError('Invalid name=val write string')
+
+    item, val = str.split('=')
+    val = int(val, 0)
+
+    old = brain[item]
+    brain[item] = val
+    print('{}: {} -> {}'.format(item, old, brain[item]))
+
+def main():
+    parser = argparse.ArgumentParser(description='Modify Brain FBL unit configuration',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog='''
+
+Read and write configuration parameters to a Brain FBL unit. A configuration
+parameter is a two-level entity, with a namespace of either "global", "setup1",
+"setup2", or "setup3". Global and setup have differing permitted fields (listed
+in fields.py).
+
+When writing, the value is a 16-bit integer which may be given in either decimal
+or hexadecimal form.
+
+Example usage:
+
+$ ./brain-config.py /dev/ttyUSB0 --read global.GovDiv
+global.GovDiv: 2100
+
+$ ./brain-config.py /dev/ttyUSB0 --write global.GovDiv=42
+global.GovDiv: 2100 -> 42
+
+$ ./brain-config.py /dev/ttyUSB0 --write global.GovDiv=0x834
+global.GovDiv: 42 -> 2100
+
+$ ./brain-config.py /dev/ttyUSB0 --read setup1.TailGainA
+setup1.TailGainA: 32767
+''')
+    parser.add_argument('device')
+    parser.add_argument('--read', action='append', help='Read a configuration parameter. E.g. global.RxType')
+    parser.add_argument('--write', action='append', help='Write a configuation parameter. Expects name=val pairs')
+    try:
+        args = parser.parse_args()
+    except:
+        print('Error: invalid arguments on command line')
+        parser.print_help()
+        sys.exit(1)
+
+    b = brain.Brain(args.device)
+
+    if args.read:
+        for parm in args.read:
+            read_item(b, parm)
+
+    if args.write:
+        for parm in args.write:
+            write_item(b, parm)
+
+if __name__ == '__main__':
+    main()
---
+++ brain.py
@@ -0,0 +1,124 @@
+import serial, struct
+from fields import SETUP_FIELDS, GLOBAL_FIELDS
+
+class Brain:
+    SETUP1 = 0
+    SETUP2 = 1
+    SETUP3 = 2
+    GLOBAL = 0xff
+
+    NAMESPACE_NAMES = {
+        'global': GLOBAL,
+        'setup1': SETUP1,
+        'setup2': SETUP2,
+        'setup3': SETUP3,
+    }
+
+    CMD_VERSION = 1
+    CMD_READ = 3
+    CMD_WRITE = 4
+
+    PKT_HEAD = b'UU'
+    PKT_TAIL = b'<'
+
+    def __init__(self, serial_name):
+        self.brain = serial.Serial(serial_name, 115200)
+        if self.version() != b'BRAIN2 3.1.010':
+            raise IOError('Incompatible Brain firmware found: {}, expect "BRAIN2 3.1.010"'.format(self.version()))
+
+    def checksum(self, data):
+        return sum(data) % 256
+
+    def create_packet(self, cmd, payload):
+        pkt = Brain.PKT_HEAD
+        pkt += b'\x00' # Sender identifier is hex 0
+        pkt += bytes([cmd])
+        pkt += payload
+        pkt += bytes([self.checksum(pkt[2:])])
+        pkt += Brain.PKT_TAIL
+        return pkt
+
+    def write_packet(self, cmd, payload=bytes([])):
+        self.brain.write(self.create_packet(cmd, payload))
+
+    def read_packet(self):
+        pkt = b''
+        while True:
+            pkt += self.brain.read(1)
+            if pkt.endswith(Brain.PKT_TAIL):
+                break
+
+        if not pkt.startswith(Brain.PKT_HEAD):
+            raise ValueError('Malformed packet header from Brain: {}'.format(pkt))
+
+        checksum = self.checksum(pkt[2:-2])
+        if checksum != pkt[-2]:
+            raise ValueError('Incorrect packet checksum from Brain: {}'.format(pkt))
+
+        if pkt[2] != 0xff:
+            raise ValueError('Response from Brain has invalid sender: {}'.format(pkt))
+
+        # Everything looks good. Return a cmd/payload pair.
+        return (pkt[3], pkt[4:-2])
+
+    def read(self, namespace, addr):
+        self.write_packet(Brain.CMD_READ, bytes([namespace, addr]))
+        cmd, resp = self.read_packet()
+        if cmd != Brain.CMD_READ:
+            raise ValueError('Brain misinterpreted read command as {}'.format(cmd))
+
+        return struct.unpack('<H', resp)[0]
+
+    def write(self, namespace, addr, data):
+        assert(data >= 0 and data <= 0xffff)
+
+        payload = bytes([namespace, addr])
+        payload += struct.pack('<H', data)
+
+        self.write_packet(Brain.CMD_WRITE, payload)
+        cmd, resp = self.read_packet()
+
+        if cmd != Brain.CMD_WRITE:
+            raise ValueError('Brain misinterpreted read command as {}'.format(cmd))
+
+        if resp[0] != namespace or resp[1] != addr:
+            raise ValueError('Brain misinterpreted read address: {} {}'.format(resp[0], resp[1]))
+
+        written_data = struct.unpack('<H', resp[2:4])[0]
+        if written_data != data:
+            raise ValueError('Brain wrote incorrect value: {}'.format(written_data))
+
+    def parse_name(self, val):
+        if val.count('.') != 1:
+            raise ValueError('Configuration parameter should have namespace and name separated by "."')
+
+        namespace, name = val.split('.')
+        if namespace not in Brain.NAMESPACE_NAMES:
+            raise ValueError('Top-level namespace should be one of "global", "setup1", "setup2" or "setup3"')
+        namespace = Brain.NAMESPACE_NAMES[namespace]
+
+        if namespace == Brain.GLOBAL:
+            if name not in GLOBAL_FIELDS:
+                raise ValueError('Configuration parameter {} not in global namespace'.format(name))
+            return (namespace, GLOBAL_FIELDS.index(name))
+        else:
+            if name not in SETUP_FIELDS:
+                raise ValueError('Configuration parameter {} not in setup namespace'.format(name))
+            return (namespace, SETUP_FIELDS.index(name))
+
+    def version(self):
+        self.write_packet(Brain.CMD_VERSION)
+        return self.read_packet()[1]
+
+    def __getitem__(self, item):
+        namespace, addr = self.parse_name(item)
+        return self.read(namespace, addr)
+
+    def __setitem__(self, item, val):
+        namespace, addr = self.parse_name(item)
+        return self.write(namespace, addr, val)
+
+if __name__ == '__main__':
+    import sys
+    b = Brain(sys.argv[1])
+    print(b['global.GovDiv'])
